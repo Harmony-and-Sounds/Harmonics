d968e49a42ab23f5ab4651155d155be14a20c8e9
juan_triana <juan_triana@javeriana.edu.co>
2020-02-24 02:12:39 -0500

    principal hecho

--- harmonics/src/components/Home/PaginaPrincipal/Home.js
+++ harmonics/src/components/Home/PaginaPrincipal/Home.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react'
 import Section from '../Section/Section'
-
+import './Home.css';
 class Home extends Component {
 
     render(){
         return (
             <div className="Home">
                 <Section/>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-sm-6 col-md-6 col-lg-6">
+                            <img className="img-responsive center" src="https://www.afandaluzas.org/wp-content/uploads/2017/07/grupo-multietnico-de-hombres-jovenes-y-de-mujeres-que-estudian-adentro_1139-989.jpg" alt="Girl in a jacket"/>
+                        </div>
+                        <div className="col my-auto">
+                            <section className="example">
+                                <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempor posuere purus quis rutrum.
+                                Proin rutrum tellus eget odio elementum, sed mollis magna accumsan. Pellentesque et diam odio.
+                                In scelerisque facilisis velit imperdiet ornare. Sed sit amet porta massa. Phasellus luctus nec elit eu finibus.
+                                Cras euismod libero nisl, sed accumsan leo tincidunt sed. Vivamus placerat odio non leo dapibus tristique.
+                                Sed at est nulla. Pellentesque condimentum id arcu semper elementum. Duis mi diam, ornare ut augue rutrum,
+                                volutpat dapibus dolor. Vivamus et tempus elit, vel dignissim diam.
+                                </p>
+                            </section>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col my-auto">
+                            <section className="example">
+                                <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempor posuere purus quis rutrum.
+                                Proin rutrum tellus eget odio elementum, sed mollis magna accumsan. Pellentesque et diam odio.
+                                In scelerisque facilisis velit imperdiet ornare. Sed sit amet porta massa. Phasellus luctus nec elit eu finibus.
+                                Cras euismod libero nisl, sed accumsan leo tincidunt sed. Vivamus placerat odio non leo dapibus tristique.
+                                Sed at est nulla. Pellentesque condimentum id arcu semper elementum. Duis mi diam, ornare ut augue rutrum,
+                                volutpat dapibus dolor. Vivamus et tempus elit, vel dignissim diam.
+                                </p>
+                            </section>
+                        </div>
+                        <div className="col-sm-6 col-md-6 col-lg-6">
+                            <img className="img-fluid center" src="https://www.afandaluzas.org/wp-content/uploads/2017/07/grupo-multietnico-de-hombres-jovenes-y-de-mujeres-que-estudian-adentro_1139-989.jpg" alt="Girl in a jacket"/>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
--- harmonics/src/components/Home/Section/Section.js
+++ harmonics/src/components/Home/Section/Section.js
@@ -13,9 +13,6 @@ class Section extends Component {
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac dignissim dolor. Cras fringilla at libero eget vehicula. 
                         Aliquam sed nunc sit amet lorem gravida lacinia sit amet laoreet nisl. Phasellus tincidunt augue vitae est rutrum elementum. 
                         Proin tincidunt porta dapibus. Sed sit amet tempus libero, pharetra fermentum dolor. Nam porttitor purus vel consequat mollis. 
-                        Nam volutpat ligula non diam lacinia faucibus. Maecenas et nulla mollis, dapibus est sit amet, feugiat leo. Aliquam et lacinia velit, 
-                        eget auctor elit. Duis arcu neque, ornare ut posuere at, auctor aliquet magna. Vestibulum egestas venenatis sapien in convallis. Etiam sed orci augue. 
-                        Maecenas eget urna tempus, fringilla sapien a, bibendum odio. Phasellus finibus risus nisl. Integer ut dui urna.
                         </p>
                     </div>
                 </section>
