a73225d13efd83f1b7c75ae8925521f5d9236037
juan_triana <juan_triana@javeriana.edu.co>
2020-03-13 01:26:08 -0500

    cambio empezar

--- harmonics/src/components/Empezar/Empezar.js
+++ harmonics/src/components/Empezar/Empezar.js
@@ -1,11 +1,12 @@
 import React, {useMemo,useState,useCallback} from 'react';
 import {useDropzone} from 'react-dropzone';
 import Multiselect from 'react-widgets/lib/Multiselect'
-import {Link} from 'react-router-dom'
+import {Link, useHistory} from 'react-router-dom'
 import 'react-widgets/dist/css/react-widgets.css';
 import './Empezar.css';
 import {obtenerDatos} from '../../servicios/servicios'
 import logo from '../../recursos/output-onlinepngtools.png'
+import { Button, Modal } from 'react-bootstrap';
 
 let instrumentos = ['Guitarra', 'Charango', 'Flauta', 'Bateria', 'Voz'];
 
@@ -37,19 +38,24 @@ const baseStyle = {
     borderColor: '#ff1744'
   };
 
-
 function Empezar(props) {
+
 
   const [value, setValue] = useState([]);
   const [archivo, setArchivo] = useState(null);
   const [nombreProy, setNombreProy] = useState('');
+  const [mostrar,setMostrar] = useState(false);
+
+  const handleClose = () => setMostrar(false);
+
+  const history = useHistory();
 
   const onDrop = useCallback(acceptedFiles => {
     // Do something with the files
     acceptedFiles.map(file => (
       getBase64(file)
   ));
-  }, [])
+  }, []);
 
   const {acceptedFiles, rejectedFiles, getRootProps, getInputProps,  isDragActive, isDragAccept, isDragReject} = useDropzone({
     accept: 'audio/mp3', //audio/mp3
@@ -86,48 +92,73 @@ function Empezar(props) {
     };
  }
 
-  return (
+ async function realizarPerticion () {
+  //Encriptar si se requiere
+  if (archivo !== null && value.length != 0 && nombreProy !== ""){
+      const respuesta = await obtenerDatos(archivo,value);
+      if (respuesta[0].name==="Colombia"){
+        setMostrar(true);
+      }
+      else{
+          alert("Error mandando la solicitud.")
+          setValue([]);
+          setArchivo(null);
+          setNombreProy("");
+      }
+  }
+  else{
+      alert("Los campos no han sido llenados correctamente.")
+  }
+} 
 
-    <div className="container-fluid vertical-center">
-      <img src={logo} alt="Harmonics" className="logoEmpezar"/>
-        <div className="fila">
-          <div className="columna_centrada">
-            <h4>Nombre del proyecto</h4>
-            <input type="text" className="form-control over" placeholder="Ingrese el nombre del proyecto" value={nombreProy} onChange={e => setNombreProy(e.target.value)} required/>
-          </div>
-        </div>
-        <div className="row">
-            <div className="col-sm-6 col-md-6 col-lg-6">
-                <div {...getRootProps({style})}>
-                    <input {...getInputProps()} />
-                    <p>Drag 'n' drop la cancion de musica andina que desa separar.</p>
-                    <em>(Solo archivos .mp3 seran permitidos.)</em>
-                </div>
-                <aside>
-                    <h4>Archivos aceptados</h4>
-                    <ul>
-                    {acceptedFilesItems}
-                    </ul>
-                </aside>
-            </div>
-            <div className="col-sm-6 col-md-6 col-lg-6">
-              <h4>Instrumentos</h4>
-              <Multiselect
-                data={instrumentos}
-                value={value}
-                onChange={(value) => setValue(value)}
-              />
-            </div>
-        </div>
-        <div className="flex-row">
-          <Link to="/instrumentos">
-            <button className="btn btn-success justify-content-center" disabled = {(archivo === null || value.length == 0)? true : false} onClick={obtenerDatos.bind(value, archivo)} >
-              Realizar serparacion
-            </button>
-          </Link>
-        </div>
-        
+function volverHome (){
+  history.push("/");
+}
+
+return (
+  <div className="container-fluid vertical-center">
+    <img src={logo} alt="Harmonics" className="logoEmpezar"/>
+    <div className="fila">
+      <div className="columna_centrada">
+        <h4>Nombre del proyecto</h4>
+        <input type="text" className="form-control over" placeholder="Ingrese el nombre del proyecto" value={nombreProy} onChange={e => setNombreProy(e.target.value)} required/>
+      </div>
     </div>
+    <div className="row">
+      <div className="col-sm-6 col-md-6 col-lg-6">
+        <div {...getRootProps({style})}>
+          <input {...getInputProps()} />
+          <p>Drag 'n' drop la cancion de musica andina que desa separar.</p>
+          <em>(Solo archivos .mp3 seran permitidos.)</em>
+        </div>
+        <aside>
+          <h4>Archivos aceptados</h4>
+          <ul>
+          {acceptedFilesItems}
+          </ul>
+        </aside>
+      </div>
+      <div className="col-sm-6 col-md-6 col-lg-6">
+        <h4>Instrumentos</h4>
+        <Multiselect data={instrumentos} value={value} onChange={(value) => setValue(value)}/>
+      </div>
+    </div>
+    <div className="flex-row">
+      <button className="btn btn-success justify-content-center" onClick={realizarPerticion}>Realizar serparacion </button>
+    </div>
+
+    <Modal show={mostrar} onHide={handleClose} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Información</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Se ha creado la solicitud de forma exitosa, se le notificara cuando su proceso alla sido completado.</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={volverHome}>
+            Volver a la pantalla principal
+          </Button>
+        </Modal.Footer>
+    </Modal>
+  </div>
   );
 }
 
--- harmonics/src/servicios/servicios.js
+++ harmonics/src/servicios/servicios.js
@@ -1,6 +1,6 @@
 export async function obtenerDatos (instrumentos ,file){
 
-    const form = new FormData()
+    /*const form = new FormData()
 
     form.append('instrumentos', instrumentos)
     form.append('file', file)
@@ -9,6 +9,18 @@ export async function obtenerDatos (instrumentos ,file){
       method: 'POST',
       headers: { 'Content-Type': 'multipart/form-data' },
       body: form,
+    })
+    return await response.json();*/
+
+    const form = new FormData()
+
+    form.append('instrumentos', instrumentos)
+    form.append('file', file)
+    
+    const response = await fetch('https://restcountries.eu/rest/v2/name/Colombia', {
+    method: 'GET',
+    /*headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: form,*/
     })
     return await response.json();
 }
