83b18c14d6f0257faba40ae18e104344ff7fa6e9
Castjuanito <juanario1997@gmail.com>
2020-04-13 16:34:31 -0500

    ajustes de diseño

--- harmonics/src/components/Home/PaginaPrincipal/Home.js
+++ harmonics/src/components/Home/PaginaPrincipal/Home.js
@@ -15,12 +15,7 @@ class Home extends Component {
                         <div className="col my-auto">
                             <section className="example">
                                 <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempor posuere purus quis rutrum.
-                                Proin rutrum tellus eget odio elementum, sed mollis magna accumsan. Pellentesque et diam odio.
-                                In scelerisque facilisis velit imperdiet ornare. Sed sit amet porta massa. Phasellus luctus nec elit eu finibus.
-                                Cras euismod libero nisl, sed accumsan leo tincidunt sed. Vivamus placerat odio non leo dapibus tristique.
-                                Sed at est nulla. Pellentesque condimentum id arcu semper elementum. Duis mi diam, ornare ut augue rutrum,
-                                volutpat dapibus dolor. Vivamus et tempus elit, vel dignissim diam.
+                                La plataforma Harmonics le permite a sus usuarios, una vez se hayan registrado, generar una pseudo-partitura, en formato .pdf, a partir de cada instrumento que seleccionen de una canción en formato .mp3. Tan sólo es necesario entrar en la sección de "Empezar" y cargar la canción que deseen, para aislar la pista que necesite de entre: voz, piano, percusión, charango, quena y guitarra.
                                 </p>
                             </section>
                         </div>
@@ -29,17 +24,24 @@ class Home extends Component {
                         <div className="col my-auto">
                             <section className="example">
                                 <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempor posuere purus quis rutrum.
-                                Proin rutrum tellus eget odio elementum, sed mollis magna accumsan. Pellentesque et diam odio.
-                                In scelerisque facilisis velit imperdiet ornare. Sed sit amet porta massa. Phasellus luctus nec elit eu finibus.
-                                Cras euismod libero nisl, sed accumsan leo tincidunt sed. Vivamus placerat odio non leo dapibus tristique.
-                                Sed at est nulla. Pellentesque condimentum id arcu semper elementum. Duis mi diam, ornare ut augue rutrum,
-                                volutpat dapibus dolor. Vivamus et tempus elit, vel dignissim diam.
+                                Una de las metas de Harmonics es apoyar en el aprendizaje de las personas, y expandir la práctica de canciones de la música andina colombiana. Con tal fin, ¡amablemente solicitamos su ayuda! Entre más utilicen la plataforma, más material estará disponible para todo visitante de la plataforma.
                                 </p>
                             </section>
                         </div>
                         <div className="col-sm-6 col-md-6 col-lg-6">
                             <img className="img-fluid center" src="https://www.afandaluzas.org/wp-content/uploads/2017/07/grupo-multietnico-de-hombres-jovenes-y-de-mujeres-que-estudian-adentro_1139-989.jpg" alt="Girl in a jacket"/>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-sm-6 col-md-6 col-lg-6">
+                            <img className="img-responsive center" src="https://www.afandaluzas.org/wp-content/uploads/2017/07/grupo-multietnico-de-hombres-jovenes-y-de-mujeres-que-estudian-adentro_1139-989.jpg" alt="Girl in a jacket"/>
+                        </div>
+                        <div className="col my-auto">
+                            <section className="example">
+                                <p>
+                                Podrá encontrar las distintas pseudo-partituras generadas por los usuarios en la sección "Proyectos." Una vez allí, podrá buscar por el instrumento que desee, o el mismo nombre de la canción; si ésta no aparece, ¡entonces puede tomar la iniciativa y agregarla! De esta manera, podemos construir una librería de pseudo-partituras en conjunto.
+                                </p>
+                            </section>
                         </div>
                     </div>
                 </div>
---
+++ harmonics/src/components/IntegranteItem/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useHistory } from "react-router-dom"
+import "./IntegranteItem.css"
+import gitLogo from "../../recursos/gitlogo.png"
+
+
+
+
+function integranteItem({src,nombre,email,git}) {
+
+
+  return (
+  <div className="integranteItemContainer">
+        <img className="integranteImg"  src={src} alt="integrante" />
+        <p></p>
+        <div className="nombreIntegrante">
+          <p>{nombre}</p>
+        </div>
+        <p><img src={gitLogo} alt="icon1" width="20" height="20" align="middle"></img> {email}</p>
+        <p><img src={gitLogo} alt="icon2" width="20" height="20" align="middle"></img> {git}</p>
+  </div>
+
+  );
+}
+
+export default integranteItem;
